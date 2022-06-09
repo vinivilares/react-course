275b0e1c356b28095706c5b12e9bdd0136b1b79f
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-06-09 16:43:36 -0300

    Scoping Component Styles With CSS Modules

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,19 +1,21 @@
 import { Link } from "react-router-dom";
 
+import classes from "./MainNavigation.module.css";
+
 function MainNavigation() {
   return (
-    <header>
-      <div>React Meetups</div>
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
           <li>
-              <Link to="/">All Meetups</Link>
+            <Link to="/">All Meetups</Link>
           </li>
           <li>
-              <Link to="/new-meetup">Add New Meetup</Link>
+            <Link to="/new-meetup">Add New Meetup</Link>
           </li>
           <li>
-              <Link to="/favorites">My Favorites</Link>
+            <Link to="/favorites">My Favorites</Link>
           </li>
         </ul>
       </nav>
