b81e10caa3f3a5150b56b9bc8fd44faa1ff96627
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-06-09 11:42:18 -0300

    Adding More Components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
 import Todo from "./components/Todo";
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
       <Todo text="Learn React" />
       <Todo text="Master React" />
       <Todo text="Explore the full React course" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
