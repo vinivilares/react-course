d440bd22347d41c118c1531821e22677d022249f
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-06-09 14:00:45 -0300

    Start Meet Up Demo Application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import Todo from "./components/Todo";
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React" />
-      <Todo text="Master React" />
-      <Todo text="Explore the full React course" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
