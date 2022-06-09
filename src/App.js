e408bea930812c8b83baa2a9b2ccc0643e027991
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-06-09 11:23:20 -0300

    Passing Data With Props and Dynamic Content

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React course" />
     </div>
   );
 }
