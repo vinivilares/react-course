0af62e2ad722afd8eca10f3bc659268ecbe02268
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-06-13 17:33:57 -0300

    Introducing React Context

---
+++ src/store/favorite-context.js
@@ -0,0 +1,21 @@
+import { createContext, useState } from "react";
+
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+});
+
+function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState();
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+  };
+
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+}
