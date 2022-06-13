fe67d518cb15e8f25dbb2784ef21d214652c6645
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-06-13 15:42:08 -0300

    Preparing the app for HTTP requests & adding a backend

--- src/pages/NewMeetupPage.js
+++ src/pages/NewMeetupPage.js
@@ -1,10 +1,14 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  function onAddMeetupHandler(meetupData) {
+    
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={onAddMeetupHandler}/>
     </section>
   );
 }
