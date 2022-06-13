13d28484cdb1cb8bab690c22436cc0a4fd70a566
Marcus Vinicius <marvi_cavalcanti@hotmail.com>
2022-06-13 15:56:47 -0300

    Sending a POST HTTP Request

--- src/pages/NewMeetupPage.js
+++ src/pages/NewMeetupPage.js
@@ -2,13 +2,22 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
   function onAddMeetupHandler(meetupData) {
-    
+    fetch(
+      "https://react-getting-started-86f31-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   }
 
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm onAddMeetup={onAddMeetupHandler}/>
+      <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
     </section>
   );
 }
